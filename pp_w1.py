3c3b77fa7c3949ad023fc66092430057e08c7fd8
Nicolas Palopoli <nicopalo@gmail.com>
2016-09-21 18:28:21 -0300

    [DONE] Completed extras.

--- pp_w1.py
+++ pp_w1.py
@@ -3,11 +3,12 @@
 import datetime
 
 name = input("Please enter your name: ")
-age = input("Please enter your age: ")
+age = int(input("Please enter your age: "))
+num = int(input("Please give me a number: "))
 
 year = datetime.date.today().year
-hundred_years = str(year + 100 - int(age))
+hundred_years = str(year + 100 - age)
 
-print("Greetings, " + name + ". You will be 100 years old in " + hundred_years + ".")
+print ( str("Greetings, " + name + ". You will be 100 years old in " + hundred_years + ".\n") * num, end="")
 
 
