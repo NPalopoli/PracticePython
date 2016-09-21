2332c4de2c97e77bc766d66cb05f7bbf196c8d63
Nicolas Palopoli <nicopalo@gmail.com>
2016-09-21 18:40:07 -0300

    [DONE] Completed basic program.

---
+++ pp_w2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+num = int(input("Please enter a number: "))
+
+if num % 2 == 0:
+  odd_or_even = "even"
+else:
+  odd_or_even = "odd"
+
+print("The number is " + odd_or_even + ".")
