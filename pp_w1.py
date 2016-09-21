62573be03561cbfb041df4717f946a390f0be501
Nicolas Palopoli <nicopalo@gmail.com>
2016-09-21 18:17:46 -0300

    [DONE] Completed basic program.

--- pp_w1.py
+++ pp_w1.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+import datetime
+
 name = input("Please enter your name: ")
-print("Your name is " + name + ".")
+age = input("Please enter your age: ")
+
+year = datetime.date.today().year
+hundred_years = str(year + 100 - int(age))
+
+print("Greetings, " + name + ". You will be 100 years old in " + hundred_years + ".")
+
+
